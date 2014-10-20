e8f15fd807ba803f02c08e98f1a8f631d4972aea
javascriptpotato <javascriptpotato@gmail.com>
2014-10-20 20:24:50 +0300

    Removing completed tasks - in progress

--- todopotato.js
+++ todopotato.js
@@ -2,6 +2,13 @@ function addText() {
     var input = document.getElementById('input').value;
     var node = document.createElement('li');
     var textnode=document.createTextNode(input);
+
+    var checkdone = document.createElement('input');
+    checkdone.setAttribute("type","checkbox");
+    node.appendChild(checkdone);
     node.appendChild(textnode);
     document.getElementById('do').appendChild(node);
+
 }
+
+
