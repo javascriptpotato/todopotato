e2d70172b5af1ad401dccd9fb1f5c0dcdfe8f99f
javascriptpotato <javascriptpotato@gmail.com>
2014-10-25 00:04:12 +0300

    Added a clock/small css changes

--- todopotato.js
+++ todopotato.js
@@ -1,3 +1,29 @@
+/* Time */
+
+function checkTime(i) {
+    if (i<10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers < 10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+}
+
+setInterval(startTime, 500);
+
+
+/* To do related */
+
 function addText() {
     var input = document.getElementById('input').value;
     var node = document.createElement('li');
